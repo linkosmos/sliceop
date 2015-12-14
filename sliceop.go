6878bae97dff24f8fe0d27397c0a5f7e167f2816
Ernestas Poskus <ernestas.poskus@gmail.com>
2015-12-14 20:49:34 +0200

    Add package sliceop

---
+++ sliceop.go
@@ -0,0 +1,62 @@
+package sliceop
+
+// Prefill - prefil array with values
+func Prefill(size int, symbol string) (output []string) {
+	output = make([]string, size)
+	for i := 0; i < size; i++ {
+		output[i] = symbol
+	}
+	return output
+}
+
+// Map - maps array of strings with func
+func Map(f func(input string) string, input ...string) (output []string) {
+	if len(input) == 0 {
+		return input
+	}
+	for _, key := range input {
+		output = append(output, f(key))
+	}
+	return output
+}
+
+// Includes - whether key includes array
+func Includes(input []string, key string) bool {
+	for _, k := range input {
+		if k == key {
+			return true
+		}
+	}
+	return false
+}
+
+// NotIncludes - whether key is not included in array
+func NotIncludes(input []string, key string) bool {
+	return !Includes(input, key)
+}
+
+// Reject - reject specified keys
+func Reject(input []string, toReject ...string) (output []string) {
+	if len(toReject) == 0 {
+		return input
+	}
+	for _, key := range input {
+		if NotIncludes(toReject, key) {
+			output = append(output, key)
+		}
+	}
+	return output
+}
+
+// Select - select and return specified keys
+func Select(input []string, toSelect ...string) (output []string) {
+	if len(toSelect) == 0 {
+		return input
+	}
+	for _, key := range input {
+		if Includes(toSelect, key) {
+			output = append(output, key)
+		}
+	}
+	return output
+}
---
+++ sliceop_test.go
@@ -0,0 +1,155 @@
+package sliceop
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var prefillTests = []struct {
+	input    int
+	symbol   string
+	expected []string
+}{
+	{9, "?", []string{"?", "?", "?", "?", "?", "?", "?", "?", "?"}},
+	{4, "#", []string{"#", "#", "#", "#"}},
+	{3, "  ", []string{"  ", "  ", "  "}},
+	{1, "0", []string{"0"}},
+	{0, "###", []string{}},
+}
+
+func TestPrefill(t *testing.T) {
+	for _, test := range prefillTests {
+		got := Prefill(test.input, test.symbol)
+		assert.Equal(t, test.expected, got)
+	}
+}
+
+var mapTests = []struct {
+	input    []string
+	expected []string
+}{
+	{[]string{"A", "b", "c"}, []string{"a", "b", "c"}},
+	{[]string{"vll", "A"}, []string{"vll", "a"}},
+	{[]string{}, []string{}},
+}
+
+func TestMap(t *testing.T) {
+	mapFunc := func(s string) string {
+		return strings.ToLower(s)
+	}
+
+	for _, test := range mapTests {
+		got := Map(mapFunc, test.input...)
+
+		assert.Equal(t, len(test.expected), len(test.input))
+		assert.Equal(t, test.expected, got)
+	}
+}
+
+var includesTests = []struct {
+	input    []string
+	key      string
+	expected bool
+}{
+	{[]string{"a", "b", "c"}, "b", true},
+	{[]string{"a", "b", "c"}, "d", false},
+	{[]string{}, "d", false},
+	{[]string{"D"}, "d", false},
+}
+
+func TestIncludes(t *testing.T) {
+	for _, test := range includesTests {
+		got := Includes(test.input, test.key)
+
+		assert.Equal(t, test.expected, got,
+			fmt.Sprintf("Expected %b for %v", test.expected, test.input))
+	}
+}
+
+var notIncludesTests = []struct {
+	input    []string
+	key      string
+	expected bool
+}{
+	{[]string{"a", "b", "c"}, "b", false},
+	{[]string{"a", "b", "c"}, "d", true},
+	{[]string{}, "d", true},
+	{[]string{"D"}, "d", true},
+}
+
+func TestNotIncludes(t *testing.T) {
+	for _, test := range notIncludesTests {
+		got := NotIncludes(test.input, test.key)
+
+		assert.Equal(t, test.expected, got,
+			fmt.Sprintf("Expected %b for %v", test.expected, test.input))
+	}
+}
+
+var rejectTests = []struct {
+	input    []string
+	toReject []string
+	expected []string
+}{
+	{
+		[]string{"A", "b", "c"},
+		[]string{"b", "c"},
+		[]string{"A"},
+	},
+	{
+		[]string{"A", "b", "c"},
+		[]string{},
+		[]string{"A", "b", "c"},
+	},
+	{
+		nil,
+		[]string{"b", "c"},
+		nil,
+	},
+}
+
+func TestReject(t *testing.T) {
+	for _, test := range rejectTests {
+		got := Reject(test.input, test.toReject...)
+
+		assert.Equal(t, test.expected, got)
+	}
+}
+
+var selectTests = []struct {
+	input    []string
+	toSelect []string
+	expected []string
+}{
+	{
+		[]string{"A", "b", "c"},
+		[]string{"b", "c"},
+		[]string{"b", "c"},
+	},
+	{
+		[]string{"A", "b", "c"},
+		[]string{"b", ""},
+		[]string{"b"},
+	},
+	{
+		[]string{"A", "b", "c"},
+		nil,
+		[]string{"A", "b", "c"},
+	},
+	{
+		nil,
+		[]string{"b", "c"},
+		nil,
+	},
+}
+
+func TestSelect(t *testing.T) {
+	for _, test := range selectTests {
+		got := Select(test.input, test.toSelect...)
+
+		assert.Equal(t, test.expected, got)
+	}
+}
